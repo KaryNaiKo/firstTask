d44774fc8ad84e9f1ca701e212f7f024c66e0d4a
karynaiko <firamar00@gmail.com>
2019-03-14 17:55:43 +0300

    first commit

---
+++ src/main/java/FirstTask.java
@@ -0,0 +1,29 @@
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.*;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FirstTask extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        List<Data> dataList = JDBCUitl.getData();
+
+        Grid<Data> grid = new Grid<>();
+        grid.setItems(dataList);
+        grid.addColumn(Data::getId).setCaption("Id");
+        grid.addColumn(Data::getData1).setCaption("Name");
+        grid.addColumn(Data::getData2).setCaption("Year of birth");
+
+        layout.addComponents(grid);
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
